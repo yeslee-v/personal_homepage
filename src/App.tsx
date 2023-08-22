6d1c956ec0e1c9fdc27f70562ab10ae7fe971d99
Yeshin Lee <yeslee5004@gmail.com>
2023-08-23 00:17:33 +0900

    배경 및 글씨 css 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 export const App: React.FC = () => {
-  return <div>예시니 홈페이지</div>;
+  return (
+    <div className="bg-slate-100 text-3xl font-bold underline">
+      예시니 홈페이지
+    </div>
+  );
 };
 
 export default App;
