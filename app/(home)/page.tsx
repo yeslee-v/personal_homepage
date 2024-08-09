41528ff012e4d4186fd66296bd731ef375846e87
Yeshin Lee <yeslee5004@gmail.com>
2024-08-09 15:22:44 +0900

    Add home: about me

---
+++ app/(home)/page.tsx
@@ -0,0 +1,16 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Home",
+};
+
+const Homepage = () => {
+  return (
+    <div>
+      <h1>Hello Everyone, I'm yeshin lee!</h1>
+      <h2>Software developer who being positive.</h2>
+    </div>
+  );
+};
+
+export default Homepage;
