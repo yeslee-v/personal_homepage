f455ef0a51f36b4ec7f36930caa61e8f2d4c5d7a
Yeshin Lee <yeslee5004@gmail.com>
2024-08-09 15:22:56 +0900

    Add not-found page

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Not Found",
+};
+
+const NotFound = () => {
+  return <h1>NOT FOUND</h1>;
+};
+
+export default NotFound;
