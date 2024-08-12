f97c4134968af13ab5db777966a8b37ee7e9a93b
Yeshin Lee <yeslee5004@gmail.com>
2024-08-12 20:28:31 +0900

    Add Projects

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from "next";
 import Career from "../../components/career";
+import Projects from "../../components/projects";
 
 export const metadata: Metadata = {
   title: "Home",
@@ -11,6 +12,7 @@ const Homepage = () => {
       <h1>Hello Everyone, I'm yeshin lee!</h1>
       <h2>Software developer who being positive.</h2>
       <Career />
+      <Projects />
     </div>
   );
 };
