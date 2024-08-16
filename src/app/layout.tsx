baf185e281cdd8043ac833cd0775f7735418f92f
Yeshin Lee <yeslee5004@gmail.com>
2024-08-16 21:17:41 +0900

    Modify declare RootLayout using arrow function

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,14 +13,12 @@ export const metadata: Metadata = {
   },
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
   );
-}
+};
+
+export default RootLayout;
